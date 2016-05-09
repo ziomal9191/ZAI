1f898d550e89dcf5e68f8919c1386042422adf75
jakubwozniczka <wozniczka.jakub@gmail.com>
2016-05-09 22:21:26 +0200

    Add db save

--- www/js/noteOperations.js
+++ www/js/noteOperations.js
@@ -21,18 +21,18 @@ function makeAndAddPhotoToDb()
 
 
 function onSuccess(imageURI) {
-    alert(imageURI);
+    //alert(imageURI);
     var db = window.sqlitePlugin.openDatabase({name: "my.db"});
-
+    var console;
     db.transaction(function(tx) {
       //tx.executeSql('DROP TABLE IF EXISTS test_table');
       tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data BLOB)');
       tx.executeSql("INSERT INTO test_table (data) VALUES (?)", [imageURI], function(tx, res) {
-          console.log("insertId: " + res.insertId + " -- probably 1");
-          console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+    	  alert( "insertId: " + res.insertId + " -- probably 1" + 
+    	  "rowsAffected: " + res.rowsAffected + " -- should be 1");
       });
     });
-    alert('SQL END');
+    //alert('SQL END' + console);
     
 }
 
