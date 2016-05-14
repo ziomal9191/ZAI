6590af969077fa9d10b42c82bf073d4e013bce40
jakubwozniczka <wozniczka.jakub@gmail.com>
2016-05-14 21:47:33 +0200

    Add filters to notes

--- www/js/noteOperations.js
+++ www/js/noteOperations.js
@@ -21,18 +21,18 @@ function makeAndAddPhotoToDb()
 
 
 function onSuccess(imageURI) {
-    //alert(imageURI);
     var db = window.sqlitePlugin.openDatabase({name: "my.db"});
     var console;
     db.transaction(function(tx) {
-      //tx.executeSql('DROP TABLE IF EXISTS test_table');
       tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data BLOB)');
-      tx.executeSql("INSERT INTO test_table (data) VALUES (?)", [imageURI], function(tx, res) {
+      tx.executeSql("INSERT INTO test_table ( data) VALUES (?)", [imageURI], function(tx, res) {
     	  alert( "insertId: " + res.insertId + " -- probably 1" + 
     	  "rowsAffected: " + res.rowsAffected + " -- should be 1");
       });
     });
-    //alert('SQL END' + console);
+	document.write('<img style=\'display:block; width:100px;height:100px;\' id=\'base64image\''+                 
+		       'src=\'data:image/jpeg;base64,\'' + imageURI + '/>');
+	
     
 }
 
@@ -41,13 +41,19 @@ function onFail(message) {
 }
 function printNotes()
 {
-	for (var i = 0; i < localStorage.length; i++){
+	 
+	 document.write('<form class="ui-filterable"><input id="filterBasic-input" data-type="search"></form>');
+	 document.write('<ul data-role="listview" data-filter="true" data-input="#filterBasic-input">');
+	 for (var i = 0; i < localStorage.length; i++){
 		var localStorageElement = localStorage.key(i);
 		if(isNaN(localStorageElement)==false)
 		{
-			document.write('<a href="#menu" data-rel="popup" data-role="button">' + localStorage.getItem(localStorageElement)+'</a>');
+			document.write('<li><a href="#menu" data-rel="popup" data-role="button">' + localStorage.getItem(localStorageElement)+'</a></li>');
+
 		}
-	}
+	 }
+	 
+	 document.write('</ul>');
 }
 function deleteNote()
 {
